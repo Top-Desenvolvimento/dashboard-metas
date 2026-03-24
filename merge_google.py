64aa5e4a01412f792b768ca0293cf5052d703085
Top-Desenvolvimento <topesteticabucal.gerencia2@gmail.com>
2026-03-24 13:27:30 -0300

    Update merge_google.py

--- merge_google.py
+++ merge_google.py
@@ -4,9 +4,9 @@ import re
 from datetime import datetime
 
 METAS_PATH = "data/metas_atual.json"
-GOOGLE_PATH = "data/google_reviews.json"
 GOOGLE_INICIAL_PATH = "data/google_inicial"
 GOOGLE_META_PATH = "data/google_metamarço"
+GOOGLE_ATUAL_PATH = "data/google_atual.json"
 
 def carregar_json(path, default):
     if not os.path.exists(path):
@@ -48,20 +48,14 @@ def descobrir_mes(base_metas):
 
 def main():
     metas = carregar_json(METAS_PATH, {})
-    google_reviews = carregar_json(GOOGLE_PATH, [])
     google_inicial = carregar_json(GOOGLE_INICIAL_PATH, {})
     google_meta = carregar_json(GOOGLE_META_PATH, {})
+    google_atual = carregar_json(GOOGLE_ATUAL_PATH, {})
 
     mes_ref = descobrir_mes(metas)
 
     base_inicial_mes = google_inicial.get(mes_ref, {})
     base_meta_mes = google_meta.get(mes_ref, {})
-
-    reviews_map = {
-        item["cidade"]: item
-        for item in google_reviews
-        if item.get("avaliacoes") is not None
-    }
 
     for cidade, info in metas.items():
         indicadores = info.setdefault("indicadores", {})
@@ -74,8 +68,7 @@ def main():
 
         inicial = extrair_numero(base_inicial_mes.get(cidade))
         meta_mes = extrair_numero(base_meta_mes.get(cidade))
-        review_item = reviews_map.get(cidade)
-        atual_total = extrair_numero(review_item.get("avaliacoes")) if review_item else None
+        atual_total = extrair_numero(google_atual.get(cidade))
 
         if meta_mes is not None:
             google_ind["meta"] = formatar_int(meta_mes)
