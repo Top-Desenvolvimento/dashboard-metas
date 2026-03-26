e86a954ed27bfa5cb4487fa97c4655db57d60e7f
Top-Desenvolvimento <topesteticabucal.gerencia2@gmail.com>
2026-03-26 15:39:14 -0300

    Update auth.js

--- docs/auth.js
+++ docs/auth.js
@@ -1,5 +1,5 @@
+alert("auth carregou");
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
 const SUPABASE_URL = "https://iahdagpmejyjspkktriw.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhaGRhZ3BtZWp5aXNwa2t0cml3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1MTExMzYsImV4cCI6MjA5MDA4NzEzNn0.gEe_4VhSEHTiK1eA_Q8UmfTYEZqH7IueT03qGLsWCCk";
 
